8b6d2f94f9af9ed6c00cba5ac982e2af16c1bd75
Qianyuan Roy Chen <qychen1027@gmail.com>
2015-10-17 23:38:52 -0400

    Router v1.0

--- Micro_router/app/controllers/students_controller.rb
+++ Micro_router/app/controllers/students_controller.rb
@@ -1,13 +1,13 @@
-require 'faraday'
 require 'net/http'
 
 class StudentsController < ApplicationController
   protect_from_forgery with: :null_session
 
   $url_ric = "http://45.55.44.135:4000/"
-  $url_student = "http://159.203.110.135:3000/"
-  $url_course = "http://localhost:5000/"
-
+  $url_router = "http://45.55.44.135:3000/"
+  $url_student = "http://159.203.110.135:80/"
+  $url_course = "http://159.203.92.173:80/"
+=begin
   #return student info back; via :get
   def info
     id1 = params[:id1]
@@ -28,56 +28,113 @@ class StudentsController < ApplicationController
 	test = request.original_url
 	@result = test
   end  
-
-  def changelastname
-  	if params[:flag]
-    	url = $url_student + params[:id1] + "/changelastname"
-    	@result = url 
-    else
-    	#resend to ric
-    	redirect_to(:action => 'ric')
-    end
-  end
-
-  def changefirstname
-  end
-
-  def addenrolled
-	path = request.path
-    id1 = params[:id1]
-    id2 = params[:id2]
-=begin
-	conn = Faraday.new(:url => $url_ric) do |faraday|
-	  faraday.request  :url_encoded             # form-encode POST params
-	  faraday.response :logger                  # log requests to STDOUT
-	  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-	end
-
-	response = conn.get 'students/addenrolled', {:id1 => id1, :id2 => id2}
-	@result = response.body
 =end
-	url = $url_ric + path
-	uri = URI(url)
+
+  def changeinfo
+	path = request.path
+	uri = URI($url_ric + path)
 	response = Net::HTTP.get_response(uri) # => String
-	result = JSON.parse(response.body)
-	#@result = result
-	#@result = result['urls']
-	result['urls'].each do |url|
+	ric_result = JSON.parse(response.body)
+
+	#@result ||= []
+	results ||= []
+	ric_result['urls'].each do |url|
+		ric_uri = JSON.parse(url)
+		if ric_uri['field'] == "student"
+			real_uri = $url_student + ric_uri['path']
+		else 
+			real_uri = $url_course + ric_uri['path']
+		end
+		uri = URI(real_uri)
+		req = Net::HTTP::Put.new(uri)
+		res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+		  http.request(req)
+		end
+		results.push(res.body)
+	end
+	#set result!!
+	@result = results[0]
+
+  end
+
+  def addcourse
+	path = request.path
+	uri = URI($url_ric + path)
+	response = Net::HTTP.get_response(uri) # => String
+	ric_result = JSON.parse(response.body)
+
+	#@result ||= []
+	results ||= []
+	ric_result['urls'].each do |url|
 		#req = Net::HTTP::Post.new(url.path)
-		#puts url
-		uri = URI(url)
-		res = Net::HTTP.post_form(uri, 'q' => 'aaa')
+		ric_uri = JSON.parse(url)
+		if ric_uri['field'] == "student"
+			real_uri = $url_student + ric_uri['path']
+		else 
+			real_uri = $url_course + ric_uri['path']
+		end
+		uri = URI(real_uri)
+		#@test = uri
+		#res = Net::HTTP.post_form(uri, 'q' => 'aaa')
+		req = Net::HTTP::Put.new(uri)
+		res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+		  http.request(req)
+		end
+		results.push(res.body)
+		#@result.push(res.body)
+		#@result.push(uri)
 	end
 
+	#set result!!
+	@result = results[0]
+
   end
 
-  def dropenrolled
+  def dropcourse
+	path = request.path
+	uri = URI($url_ric + path)
+	response = Net::HTTP.get_response(uri) # => String
+	ric_result = JSON.parse(response.body)
+
+	#@result ||= []
+	results ||= []
+	ric_result['urls'].each do |url|
+		#req = Net::HTTP::Post.new(url.path)
+		ric_uri = JSON.parse(url)
+		if ric_uri['field'] == "student"
+			real_uri = $url_student + ric_uri['path']
+		else 
+			real_uri = $url_course + ric_uri['path']
+		end
+		uri = URI(real_uri)
+		#@test = uri
+		#res = Net::HTTP.post_form(uri, 'q' => 'aaa')
+		req = Net::HTTP::Delete.new(uri)
+		res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+		  http.request(req)
+		end
+		results.push(res.body)
+		#@result.push(res.body)
+		#@result.push(uri)
+	end
+
+	#set result!!
+	@result = results[0]
   end
 
-  def addtaken
+  def doget
+	path = request.path
+	uri = URI($url_student + path)
+	response = Net::HTTP.get_response(uri) # => String
+	@result = response.body
+
   end
 
-  def droptaken
+  
+  def addcourselist
+  end
+
+  def dropcourselist
   end
 
 
--- Micro_router/config/routes.rb
+++ Micro_router/config/routes.rb
@@ -9,9 +9,47 @@ Rails.application.routes.draw do
 
   #match '/students(/:id1(/:action(/:id2)))' => 'students#ric_get', via: :get
 
-  #get 'courses/add', :to => "courses#create"
+  # ------------  Students ------------------------
 
-  match ':controller(/:id1(/:action(/:id2)))', :via => [:post, :get]
+  put "students/:id1/coursesEnrolled/:id2" => "students#addcourse"
+  delete "students/:id1/coursesEnrolled/:id2" => "students#dropcourse"
+  put "students/:id1/coursesEnrolled" => "students#addcourselist"
+  delete "students/:id1/coursesEnrolled" => "students#dropcourselist"
+
+  put "students/:id1/coursesTaken/:id2" => "students#addcourse"
+  delete "students/:id1/coursesTaken/:id2" => "students#dropcourse"
+  put "students/:id1/coursesTaken" => "students#addcourselist"
+  delete "students/:id1/coursesTaken" => "students#dropcourselist"
+
+  put "students/:id1/lastName" => "students#changeinfo"
+  put "students/:id1/firstName" => "students#changeinfo"
+  put "students/:id1" => "students#changeinfo"
+
+ # post "students/"
+
+  get "students/:id1/lastName" => "students#doget"
+  get "students/:id1/firstName" => "students#doget"
+  get "students/:id1" => "students#doget"
+  get "students/:id1/coursesTaken" => "students#doget"
+  get "students/:id1/coursesEnrolled" => "students#doget"
+
+
+  # -------------  Courses ------------------------
+
+  put "courses/:id1/students/:id2" => "courses#addstudent"
+  delete "courses/:id1/students/:id2" => "courses#dropstudent"
+
+  put "courses/:id1/room" => "courses#changeinfo"
+  put "courses/:id1/title" => "courses#changeinfo"
+  put "courses/:id1" => "courses#changeinfo"
+
+  get "courses/:id1/room" => "courses#doget"
+  get "courses/:id1/title" => "courses#doget"
+  get "courses/:id1/students" => "courses#doget"
+  get "courses/:id1" => "courses#doget"
+
+  match 'students(/:id1(/:action(/:id2)))', :to => 'students#doget', :via => [:post, :get]
+  match 'courses(/:id1(/:action(/:id2)))', :to => 'courses#doget', :via => [:post, :get]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
