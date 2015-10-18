30fc83122bf0cd585f97a783171c36edb83ed87c
qychen1027 <qychen1027@gmail.com>
2015-10-17 23:30:06 -0400

    RIC v1.0

--- Micro_RIC/RIC/app/controllers/students_controller.rb
+++ Micro_RIC/RIC/app/controllers/students_controller.rb
@@ -5,15 +5,44 @@ class StudentsController < ApplicationController
   $url_student = "http://159.203.110.135:3000/"
   $url_course = "http://localhost:5000/"
 
-  #/students/4/addenrolled/2
-  def addenrolled
+  #/students/4/coursesEnrolled/2
+  def coursesEnrolled
     path = request.path
     studentid = params[:id1]
     courseid = params[:id2]
-    url1 = $url_student + path
-    url2 = $url_course + "courses/" + courseid + "/addstudent/" + studentid
+    #field ||= [].push('student')
+    url1 = '{"field": "student", "path": "' + path + '"}'
+    url2 = '{"field": "course", "path": "' + 'courses/' + courseid + "/students/" + studentid + '"}'
     (urls ||= []).push(url1)
     urls.push(url2)
+
+    respond_to do |format|
+      msg = { :status => "ok", :urls => urls }
+      format.json  { render :json => msg } # don't do msg.to_json
+    end
+  end
+
+  #/students/4/coursesTaken/2
+  def coursesTaken
+    path = request.path
+    studentid = params[:id1]
+    courseid = params[:id2]
+    #field ||= [].push('student')
+    url1 = '{"field": "student", "path": "' + path + '"}'   
+    (urls ||= []).push(url1)
+
+    respond_to do |format|
+      msg = { :status => "ok", :urls => urls }
+      format.json  { render :json => msg } # don't do msg.to_json
+    end
+  end
+
+  #/students/4(/last/firstname)
+  def changeinfo
+    path = request.path
+    studentid = params[:id1]
+    url1 = '{"field": "student", "path": "' + path + '"}'
+    (urls ||= []).push(url1)
 
     respond_to do |format|
       msg = { :status => "ok", :urls => urls }
